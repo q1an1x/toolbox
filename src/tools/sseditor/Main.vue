3aeac4f4f93416c77bf4360d2625f278add456e9
etherein <xinqian@live.com>
2022-11-16 05:02:46 +0800

    initial commit

---
+++ src/tools/sseditor/Main.vue
@@ -0,0 +1,174 @@
+<template>
+  <div class="fullscreen app">
+    <menu-bar class="bar" @copy="copyToClipboard(input)" />
+
+    <q-input
+      autogrow
+      autofocus
+      borderless
+      v-model="input"
+      class="input"
+      ref="input"
+      input-style="line-height: 2rem"
+      @update:model-value="update"
+      @keydown.delete="mode = 0"
+    />
+  </div>
+</template>
+
+<script>
+import MenuBar from "src/tools/sseditor/components/MenuBar";
+export default {
+  name: "sseditorMain",
+  components: { MenuBar },
+  data() {
+    return {
+      input: '',
+      previous: '',
+      mode: 0,
+      dismissIndicator: () => {}
+    }
+  },
+
+  watch: {
+    mode(value) {
+      this.dismissIndicator();
+      switch (value) {
+        case 1:
+          this.dismissIndicator = this.$q.notify({
+            color: 'info',
+            message: 'entering subscripts',
+            timeout: 0
+          });
+          return;
+        case 2:
+          this.dismissIndicator = this.$q.notify({
+            color: 'warning',
+            message: 'entering superscripts',
+            timeout: 0
+          });
+          return;
+      }
+    }
+  },
+
+  methods: {
+    update() {
+      const char = this.input.replace(this.previous, '');
+
+      if (char === '_') {
+        this.$nextTick(() => {
+          this.input = this.previous;
+        });
+        this.mode = 1;
+        return;
+      }
+      if (char === '^') {
+        this.$nextTick(() => {
+          this.input = this.previous;
+        });
+        this.mode = 2;
+        return;
+      }
+      if (this.mode === 1 || this.mode === 2) {
+        if (char === " ") {
+          this.$nextTick(() => {
+            this.input = this.previous;
+          });
+          this.mode = 0;
+          return;
+        }
+
+        this.input = this.previous;
+        if (char === "\r\n" || char === "\n" || char === "\r") {
+          this.mode = 0;
+          return;
+        }
+
+        this.input += this.mode === 1 ? this.getSub(char) : this.getSuper(char);
+      }
+
+      this.previous = this.input;
+    },
+
+    copyToClipboard(text) {
+      function fallbackCopyTextToClipboard(text) {
+        var textArea = document.createElement("textarea");
+        textArea.value = text;
+        textArea.style.top = "0";
+        textArea.style.left = "0";
+        textArea.style.position = "fixed";
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+        try {
+          document.execCommand('copy');
+        } catch (err) {}
+        document.body.removeChild(textArea);
+      }
+      function copyTextToClipboard(text) {
+        if (!navigator.clipboard) {
+          fallbackCopyTextToClipboard(text);
+          return;
+        }
+        navigator.clipboard.writeText(text);
+      }
+      copyTextToClipboard(text);
+      this.$q.notify({
+        timeout: 2,
+        message: 'Copied to clipboard.'
+      });
+    },
+
+    getSuper(char) {
+      return {
+        '0': '⁰', '1': '¹', '2': '²', '3': '³', '4': '⁴', '5': '⁵', '6': '⁶', '7': '⁷', '8': '⁸',
+        '9': '⁹', '+': '⁺', '-': '⁻', '=': '⁼', '(': '⁽', ')': '⁾',
+        'a': 'ᵃ', 'b': 'ᵇ', 'c': 'ᶜ', 'd': 'ᵈ', 'e': 'ᵉ', 'f': 'ᶠ', 'g': 'ᶢ', 'h': 'ʰ', 'i': 'ⁱ',
+        'j': 'ʲ', 'k': 'ᵏ', 'l': 'ˡ', 'm': 'ᵐ', 'n': 'ⁿ', 'o': 'ᵒ', 'p': 'ᵖ', /* q */ 'r': 'ʳ',
+        's': 'ˢ', 't': 'ᵗ', 'u': 'ᵘ', 'v': 'ᵛ', 'w': 'ʷ', 'x': 'ˣ', 'y': 'ʸ', 'z': 'ᶻ',
+        'A': 'ᴬ', 'B': 'ᴮ', 'C': 'ꟲ', 'D': 'ᴰ', 'E': 'ᴱ', 'F': 'ꟳ', 'G': 'ᴳ', 'H': 'ᴴ', 'I': 'ᴵ',
+        'J': 'ᴶ', 'K': 'ᴷ', 'L': 'ᴸ', 'M': 'ᴹ', 'N': 'ᴺ', 'O': 'ᴼ', 'P': 'ᴾ', 'Q': 'ꟴ', 'R': 'ᴿ',
+        'S': 'ˢ', 'T': 'ᵀ', 'U': 'ᵁ', 'V': 'ⱽ', 'W': 'ᵂ', 'X': 'ˣ', 'Y': 'ʸ', 'Z': 'ᶻ' // s, x, y, z does not have uppercase
+      }[char] ?? char;
+    },
+
+    getSub(char) {
+      return {
+        '0': '₀', '1': '₁', '2': '₂', '3': '₃', '4': '₄', '5': '₅', '6': '₆', '7': '₇', '8': '₈',
+        '9': '₉', '+': '₊', '-': '₋', '=': '₌', '(': '₍', ')': '₎',
+        'a': 'ₐ',/* b, c, d */ 'e': 'ₑ', /* f, g */ 'h': 'ₕ', 'i': 'ᵢ',
+        'j': 'ⱼ', 'k': 'ₖ', 'l': 'ₗ', 'm': 'ₘ', 'n': 'ₙ', 'o': 'ₒ', 'p': 'ₚ', /* q */ 'r': 'ᵣ',
+        's': 'ₛ', 't': 'ₜ', 'u': 'ᵤ', 'v': 'ᵥ', /* w */ 'x': 'ₓ', /* y, z */ // subscript misses a lot of characters...
+        // and no uppercase at all
+        // todo: option to convert uppercase to lowercase subscripts
+      }[char] ?? char;
+    }
+  },
+}
+</script>
+
+<style scoped>
+.input {
+  padding: 0 1rem;
+  width: 100%;
+  height: 100%;
+  font-size: 1.5rem;
+  cursor: text !important;
+  line-height: 122rem !important;
+}
+.app {
+  cursor: text !important;
+}
+.bar {
+  position: absolute;
+  right: 25px;
+  top: 15px;
+  z-index: 5;
+}
+</style>
+<style>
+.q-notification {
+  transition: 0.3s;
+}
+</style>
---
+++ src/tools/sseditor/components/MenuBar.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="q-gutter-x-sm">
+    <q-btn round color="white" text-color="black" :icon="matCopyAll" @click="$emit('copy')" />
+    <q-btn round color="white" text-color="black" :icon="matInfo" @click="showInfo" />
+    <q-btn round color="white" text-color="black" :icon="matSettings" @click="showSettings" />
+  </div>
+</template>
+
+<script>
+import { matSettings, matInfo, matCopyAll } from "@quasar/extras/material-icons"
+
+export default {
+  name: "MenuBar",
+
+  setup() {
+    return {
+      matSettings, matInfo, matCopyAll
+    }
+  },
+
+  methods: {
+    showInfo() {
+      this.$q.dialog({
+        title: 'usage',
+        color: 'black',
+        message: `caret(^) for superscripts<br/>
+        underline(_) for subscripts<br/>
+        space/delete for normal characters<br/>
+        that is it...<br/><br/>
+        example: to write ²H₂O, you type: ^2 H_2 O<br/><br/>
+        note that this tool does it by converting characters to the superscript/subscript version of these characters(with different codes)
+        and should work on most modern platforms regardless of programs/apps used, however, not all characters will display as intended and do not expect consistency<br/><br/>
+        read: <a href="https://en.wikipedia.org/wiki/Unicode_subscripts_and_superscripts" target="_blank">wikipedia: unicode_subscripts_and_superscripts</a>`,
+        html: true
+      });
+    },
+    showSettings() {
+      this.$q.dialog({
+        title: 'settings',
+        color: 'black',
+        options: {
+          type: 'checkbox',
+          model: [],
+          items: [
+            { label: 'auto-copy', value: 'autoCopy' }
+          ]
+        }
+      });
+    }
+  }
+}
+</script>
+<style>
+a {
+  color: black;
+}
+</style>
